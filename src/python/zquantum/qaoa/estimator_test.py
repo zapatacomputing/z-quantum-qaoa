5a83bd77f113137dbd3f86a5e7abc5cb127db3b7
mstechly <michal.stechly@gmail.com>
2021-01-26 18:28:33 -0500

    Added cvar estimator.

---
+++ src/python/zquantum/qaoa/estimator_test.py
@@ -0,0 +1,122 @@
+import pytest
+from pyquil import Program
+from pyquil.gates import X, H
+from openfermion import QubitOperator, qubit_operator_sparse, IsingOperator
+import numpy as np
+
+from zquantum.core.interfaces.estimator_test import EstimatorTests
+from zquantum.core.interfaces.mock_objects import (
+    MockQuantumBackend,
+    MockQuantumSimulator,
+)
+from zquantum.core.estimator_test import TestBasicEstimator
+from zquantum.core.circuit import Circuit
+from zquantum.core.measurement import Measurements, ExpectationValues
+from .estimators import CvarEstimator
+
+
+class TestCvarEstimator(EstimatorTests):
+    @pytest.fixture(
+        params=[
+            {"alpha": 1.0},
+            {"alpha": 0.8},
+            {"alpha": 0.5},
+            {"alpha": 0.2},
+        ]
+    )
+    def estimator(self, request):
+        return CvarEstimator(**request.param)
+
+    @pytest.fixture()
+    def operator(self, request):
+        return IsingOperator("Z0")
+
+    @pytest.fixture()
+    def circuit(self, request):
+        return Circuit(Program(X(0)))
+
+    @pytest.fixture()
+    def backend(self, request):
+        backend = MockQuantumBackend()
+
+        def custom_run_circuit_and_measure(circuit):
+            bitstrings = [("0"), ("1")]
+            return Measurements(bitstrings)
+
+        backend.run_circuit_and_measure = custom_run_circuit_and_measure
+        return backend
+
+    @pytest.fixture()
+    def n_samples(self, request):
+        return 10
+
+    @pytest.fixture()
+    def epsilon(self, request):
+        return None
+
+    @pytest.fixture()
+    def delta(self, request):
+        return None
+
+    def test_get_estimated_expectation_values_returns_expectation_values(
+        self, estimator, backend, circuit, operator
+    ):
+        value = estimator.get_estimated_expectation_values(
+            backend=backend,
+            circuit=circuit,
+            target_operator=operator,
+        )
+        # Then
+        assert type(value) is ExpectationValues
+
+    def test_raises_exception_if_operator_is_not_ising(
+        self, estimator, backend, circuit
+    ):
+        # Given
+        operator = QubitOperator("X0")
+        with pytest.raises(TypeError):
+            value = estimator.get_estimated_expectation_values(
+                backend,
+                circuit,
+                operator,
+            ).values
+
+    def test_cvar_estimator_raises_exception_if_alpha_less_than_0(
+        self, backend, circuit, operator
+    ):
+        estimator = CvarEstimator(alpha=-1)
+        with pytest.raises(ValueError):
+            value = estimator.get_estimated_expectation_values(
+                backend,
+                circuit,
+                operator,
+            ).values
+
+    def test_cvar_estimator_raises_exception_if_alpha_greater_than_1(
+        self, backend, circuit, operator
+    ):
+        estimator = CvarEstimator(alpha=2)
+        with pytest.raises(ValueError):
+            value = estimator.get_estimated_expectation_values(
+                backend,
+                circuit,
+                operator,
+            ).values
+
+    def test_cvar_estimator_returns_correct_values(self, estimator, backend, operator):
+        # Given
+        circuit = Circuit(Program(H(0)))
+        # This n_samples value comes from
+        alpha = estimator.alpha
+        if alpha <= 0.5:
+            target_value = -1
+        else:
+            target_value = (-1 * 0.5 + 1 * (alpha - 0.5)) / alpha
+
+        # When
+        value = estimator.get_estimated_expectation_values(
+            backend, circuit, operator
+        ).values
+
+        # Then
+        assert value == pytest.approx(target_value)
---
+++ src/python/zquantum/qaoa/estimators.py
@@ -0,0 +1,84 @@
+import numpy as np
+from openfermion import IsingOperator
+from typing import Optional
+from overrides import overrides
+
+from zquantum.core.circuit import Circuit
+from zquantum.core.interfaces.backend import QuantumBackend, QuantumSimulator
+from zquantum.core.measurement import ExpectationValues, Measurements
+from zquantum.core.interfaces.estimator import Estimator
+
+
+class CvarEstimator(Estimator):
+    """An estimator for calculating expectation value using CVaR method.
+    The main idea is that for diagonal operators the ground state of the Hamiltonian is a base state.
+    In particular for the combinatorial optimization problems, we care only about getting a single bitstring representing the solution.
+    Therefore, it might be beneficial for the optimization process to discard samples that represent inferior solutions.
+    CVaR Estimator takes uses only a top X percentile of the samples for calculating the expectation value, where X = alpha*100.
+
+    Reference: https://arxiv.org/abs/1907.04769
+    "Improving Variational Quantum Optimization using CVaR", P. Barkoutsos, G. Nannicini, A. Robert, I. Tavernelli, and S. Woerner
+    """
+
+    def __init__(self, alpha):
+        super().__init__()
+        self.alpha = alpha
+
+    @overrides
+    def get_estimated_expectation_values(
+        self,
+        backend: QuantumBackend,
+        circuit: Circuit,
+        target_operator: IsingOperator,
+        n_samples: Optional[int] = None,
+    ) -> ExpectationValues:
+        """Given a circuit, backend, and target operators, this method produces expectation values
+        using CVaR algorithm.
+
+        Args:
+            backend (QuantumBackend): the backend that will be used to run the circuit
+            circuit (Circuit): the circuit that prepares the state.
+            target_operator (SymbolicOperator): Operator to be estimated.
+            alpha (float): defines what part of the best measurements should be taken into account in the estimation process.
+            n_samples (int): Number of measurements done on the unknown quantum state.
+
+        Raises:
+            AttributeError: If backend is not a QuantumSimulator.
+
+        Returns:
+            ExpectationValues: expectation values for each term in the target operator.
+        """
+        if self.alpha > 1 or self.alpha <= 0:
+            raise ValueError("alpha needs to be a value between 0 and 1.")
+
+        if type(target_operator) is not IsingOperator:
+            raise TypeError("Operator should be of type IsingOperator.")
+
+        # TODO: pseudocode
+        measurements = backend.run_circuit_and_measure(circuit)
+        distribution = measurements.get_distribution()
+        expected_values_per_bitstring = {}
+
+        for bitstring in distribution.distribution_dict:
+            expected_value = Measurements(bitstring).get_expectation_values(
+                target_operator
+            )
+            expected_values_per_bitstring[bitstring] = expected_value.values[0]
+
+        sorted_expected_values_per_bitstring_list = sorted(
+            expected_values_per_bitstring.items(), key=lambda item: item[1]
+        )
+
+        cumulative_prob = 0.0
+        cumulative_value = 0.0
+
+        for bitstring, energy in sorted_expected_values_per_bitstring_list:
+            prob = distribution.distribution_dict[bitstring]
+            if cumulative_prob + prob < self.alpha:
+                cumulative_prob += prob
+                cumulative_value += prob * energy
+            else:
+                cumulative_value += (self.alpha - cumulative_prob) * energy
+                break
+        final_value = cumulative_value / self.alpha
+        return ExpectationValues(final_value)
