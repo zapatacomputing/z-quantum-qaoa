eb41b606cd948b9b4f8e664ca8165d7e33742fa7
mstechly <michal.stechly@gmail.com>
2021-03-17 14:52:24 -0400

    Refactored tests for warm start QAOA ansatz.

--- src/python/zquantum/qaoa/ansatzes/warm_start_ansatz_test.py
+++ src/python/zquantum/qaoa/ansatzes/warm_start_ansatz_test.py
@@ -9,40 +9,37 @@ import numpy as np
 import sympy
 
 
-class TestWarmStartQAOAAnsatz(AnsatzTests):
-    @pytest.fixture
-    def thetas(self):
-        return np.array([0.5, 0.5])
+def create_betas(number_of_layers):
+    return list(sympy.symbols(f"beta_:{number_of_layers}"))
 
-    @pytest.fixture
-    def ansatz(self, thetas):
-        cost_hamiltonian = QubitOperator((0, "Z")) + QubitOperator((1, "Z"))
-        return WarmStartQAOAAnsatz(
-            number_of_layers=1,
-            cost_hamiltonian=cost_hamiltonian,
-            thetas=thetas,
-        )
 
-    @pytest.fixture
-    def beta(self):
-        return sympy.Symbol("beta_0")
+def create_gammas(number_of_layers):
+    return list(sympy.symbols(f"gamma_:{number_of_layers}"))
 
-    @pytest.fixture
-    def gamma(self):
-        return sympy.Symbol("gamma_0")
 
-    @pytest.fixture
-    def symbols_map(self, beta, gamma):
-        beta_value = 0.5
-        gamma_value = 0.7
-        return [(beta, beta_value), (gamma, gamma_value)]
+def create_symbols_map(number_of_layers):
+    betas = create_betas(number_of_layers)
+    gammas = create_gammas(number_of_layers)
+    symbols_map = []
+    beta_value = 0.5
+    gamma_value = 0.7
+    for beta, gamma in zip(betas, gammas):
+        symbols_map.append([beta, beta_value])
+        symbols_map.append([gamma, gamma_value])
+    return symbols_map
 
-    @pytest.fixture
-    def target_unitary(self, beta, gamma, thetas, symbols_map):
-        target_circuit = Circuit()
-        target_circuit.gates = []
-        target_circuit.gates.append(Gate("Ry", [Qubit(0)], [thetas[0]]))
-        target_circuit.gates.append(Gate("Ry", [Qubit(1)], [thetas[1]]))
+
+def create_target_unitary(thetas, number_of_layers):
+    target_circuit = Circuit()
+    target_circuit.gates = []
+    target_circuit.gates.append(Gate("Ry", [Qubit(0)], [thetas[0]]))
+    target_circuit.gates.append(Gate("Ry", [Qubit(1)], [thetas[1]]))
+    betas = create_betas(number_of_layers)
+    gammas = create_gammas(number_of_layers)
+    symbols_map = create_symbols_map(number_of_layers)
+    for layer_id in range(number_of_layers):
+        beta = betas[layer_id]
+        gamma = gammas[layer_id]
         target_circuit.gates.append(Gate("Rz", [Qubit(0)], [2.0 * gamma]))
         target_circuit.gates.append(Gate("Rz", [Qubit(1)], [2.0 * gamma]))
         target_circuit.gates.append(Gate("Ry", [Qubit(0)], [-thetas[0]]))
@@ -52,7 +49,26 @@ class TestWarmStartQAOAAnsatz(AnsatzTests):
         target_circuit.gates.append(Gate("Ry", [Qubit(0)], [thetas[0]]))
         target_circuit.gates.append(Gate("Ry", [Qubit(1)], [thetas[1]]))
 
-        return target_circuit.evaluate(symbols_map).to_unitary()
+    return target_circuit.evaluate(symbols_map).to_unitary()
+
+
+class TestWarmStartQAOAAnsatz(AnsatzTests):
+    @pytest.fixture
+    def thetas(self):
+        return np.array([0.5, 0.5])
+
+    @pytest.fixture(params=[1, 2, 3])
+    def number_of_layers(self, request):
+        return request.param
+
+    @pytest.fixture
+    def ansatz(self, thetas, number_of_layers):
+        cost_hamiltonian = QubitOperator((0, "Z")) + QubitOperator((1, "Z"))
+        return WarmStartQAOAAnsatz(
+            number_of_layers=number_of_layers,
+            cost_hamiltonian=cost_hamiltonian,
+            thetas=thetas,
+        )
 
     def test_set_cost_hamiltonian(self, ansatz):
         # Given
@@ -121,24 +137,29 @@ class TestWarmStartQAOAAnsatz(AnsatzTests):
         # Then
         assert ansatz.number_of_qubits == target_number_of_qubits
 
-    def test_get_parametrizable_circuit(self, ansatz, beta, gamma):
+    def test_get_parametrizable_circuit(self, ansatz, number_of_layers):
         # Then
-        assert ansatz.parametrized_circuit.symbolic_params == [
-            gamma,
-            beta,
-        ]
+        betas = create_betas(number_of_layers)
+        gammas = create_gammas(number_of_layers)
+        target_params = []
+        for i in range(number_of_layers):
+            target_params.append(gammas[i])
+            target_params.append(betas[i])
 
-    def test_generate_circuit(self, ansatz, symbols_map, target_unitary):
+        assert ansatz.parametrized_circuit.symbolic_params == target_params
+
+    def test_generate_circuit(self, ansatz, number_of_layers, thetas):
         # When
+        symbols_map = create_symbols_map(number_of_layers)
         parametrized_circuit = ansatz._generate_circuit()
         evaluated_circuit = parametrized_circuit.evaluate(symbols_map)
         final_unitary = evaluated_circuit.to_unitary()
-
+        target_unitary = create_target_unitary(thetas, number_of_layers)
         # Then
         assert compare_unitary(final_unitary, target_unitary, tol=1e-10)
 
     def test_generate_circuit_with_ising_operator(
-        self, ansatz, symbols_map, target_unitary
+        self, ansatz, number_of_layers, thetas
     ):
         # When
         ansatz.cost_hamiltonian = change_operator_type(
@@ -146,6 +167,8 @@ class TestWarmStartQAOAAnsatz(AnsatzTests):
         )
 
         parametrized_circuit = ansatz._generate_circuit()
+        symbols_map = create_symbols_map(number_of_layers)
+        target_unitary = create_target_unitary(thetas, number_of_layers)
         evaluated_circuit = parametrized_circuit.evaluate(symbols_map)
         final_unitary = evaluated_circuit.to_unitary()
 
