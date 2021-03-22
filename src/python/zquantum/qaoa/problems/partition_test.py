59dc84687f524a750b55861fd6e97f360898b494
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-22 19:29:54 +0100

    Add graph partition test with minimal example

--- src/python/zquantum/qaoa/problems/partition_test.py
+++ src/python/zquantum/qaoa/problems/partition_test.py
@@ -1,16 +1,30 @@
 import networkx as nx
-from zquantum.qaoa.farhi_ansatz import QAOAFarhiAnsatz
+import pytest
 
 from .partition import get_graph_partition_hamiltonian
 
-class TestGetHamiltonian:
-    def test_sanity(self):
-        G = nx.Graph()
-        G.add_nodes_from([0, 1, 2, 3])
-        G.add_edge(0, 1, weight=10)
-        G.add_edge(0, 3, weight=10)
-        G.add_edge(1, 2, weight=1)
-        G.add_edge(2, 3, weight=1)
-        qubit_operator = get_graph_partition_hamiltonian(G)
-        assert qubit_operator is None
 
+def _make_graph(adj_dict):
+    graph = nx.Graph()
+    for src_node, dest_nodes in adj_dict.items():
+        for dest_node in dest_nodes:
+            graph.add_edge(src_node, dest_node)
+    return graph
+
+
+GRAPH_OPERATOR_TERM_PAIRS = [
+    (
+        _make_graph({0: [1]}),
+        {
+            (): 2.5+0j,
+            ((0, 'Z'), (1, 'Z')): 1.5+0j
+        }
+    ),
+]
+
+
+class TestGetHamiltonian:
+    @pytest.mark.parametrize('graph,terms', GRAPH_OPERATOR_TERM_PAIRS)
+    def test_returns_expected_terms(self, graph, terms):
+        qubit_operator = get_graph_partition_hamiltonian(graph)
+        assert qubit_operator.terms == terms
