a7598d459f33b5c65b30b5130b3d87ae77c7fe51
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-23 11:16:10 +0100

    Add more examples calculated by hand to graph partition tests

--- src/python/zquantum/qaoa/problems/partition_test.py
+++ src/python/zquantum/qaoa/problems/partition_test.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import pytest
+# from zquantum.qaoa.farhi_ansatz import QAOAFarhiAnsatz
 
 from .partition import get_graph_partition_hamiltonian
 
@@ -12,14 +13,42 @@ def _make_graph(adj_dict):
     return graph
 
 
+def _graph_from_edges(edges):
+    graph = nx.Graph()
+    for edge in edges:
+        graph.add_edge(*edge)
+    return graph
+
+
 GRAPH_OPERATOR_TERM_PAIRS = [
     (
         _make_graph({0: [1]}),
         {
             (): 2.5+0j,
-            ((0, 'Z'), (1, 'Z')): 1.5+0j
+            ((0, 'Z'), (1, 'Z')): 1.5+0j,
         }
     ),
+    (
+        _graph_from_edges([(0, 1), (0, 2)]),
+        {
+            (): 4+0j,
+            ((0, 'Z'), (1, 'Z')): 1.5+0j,
+            ((0, 'Z'), (2, 'Z')): 1.5+0j,
+            ((1, 'Z'), (2, 'Z')): 2.0+0j,
+        }
+    ),
+    (
+        _make_graph({0: [1, 2, 3]}),
+        {
+            (): 5.5+0j,
+            ((0, 'Z'), (1, 'Z')): 1.5+0j,
+            ((0, 'Z'), (2, 'Z')): 1.5+0j,
+            ((0, 'Z'), (3, 'Z')): 1.5+0j,
+            ((1, 'Z'), (2, 'Z')): 2+0j,
+            ((1, 'Z'), (3, 'Z')): 2+0j,
+            ((2, 'Z'), (3, 'Z')): 2+0j,
+        }
+    )
 ]
 
 
