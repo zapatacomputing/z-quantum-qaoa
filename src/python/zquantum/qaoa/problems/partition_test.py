18bf7ab9477d7ba72586e97430f7aa50db537fe9
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-23 11:37:37 +0100

    Add graph example

--- src/python/zquantum/qaoa/problems/partition_test.py
+++ src/python/zquantum/qaoa/problems/partition_test.py
@@ -1,16 +1,7 @@
 import networkx as nx
 import pytest
-# from zquantum.qaoa.farhi_ansatz import QAOAFarhiAnsatz
 
 from .partition import get_graph_partition_hamiltonian
-
-
-def _make_graph(adj_dict):
-    graph = nx.Graph()
-    for src_node, dest_nodes in adj_dict.items():
-        for dest_node in dest_nodes:
-            graph.add_edge(src_node, dest_node)
-    return graph
 
 
 def _graph_from_edges(edges):
@@ -22,7 +13,7 @@ def _graph_from_edges(edges):
 
 GRAPH_OPERATOR_TERM_PAIRS = [
     (
-        _make_graph({0: [1]}),
+        _graph_from_edges([(0, 1)]),
         {
             (): 2.5+0j,
             ((0, 'Z'), (1, 'Z')): 1.5+0j,
@@ -38,7 +29,7 @@ GRAPH_OPERATOR_TERM_PAIRS = [
         }
     ),
     (
-        _make_graph({0: [1, 2, 3]}),
+        _graph_from_edges([(0, 1), (0, 2), (0, 3)]),
         {
             (): 5.5+0j,
             ((0, 'Z'), (1, 'Z')): 1.5+0j,
@@ -47,6 +38,22 @@ GRAPH_OPERATOR_TERM_PAIRS = [
             ((1, 'Z'), (2, 'Z')): 2+0j,
             ((1, 'Z'), (3, 'Z')): 2+0j,
             ((2, 'Z'), (3, 'Z')): 2+0j,
+        }
+    ),
+    (
+        _graph_from_edges([(0, 1), (1, 2), (3, 4)]),
+        {
+            (): 6.5+0j,
+            ((0, 'Z'), (1, 'Z')): 1.5+0j,
+            ((0, 'Z'), (2, 'Z')): 2+0j,
+            ((0, 'Z'), (3, 'Z')): 2+0j,
+            ((0, 'Z'), (4, 'Z')): 2+0j,
+            ((1, 'Z'), (2, 'Z')): 1.5+0j,
+            ((1, 'Z'), (3, 'Z')): 2+0j,
+            ((1, 'Z'), (4, 'Z')): 2+0j,
+            ((2, 'Z'), (3, 'Z')): 2+0j,
+            ((2, 'Z'), (4, 'Z')): 2+0j,
+            ((3, 'Z'), (4, 'Z')): 1.5+0j,
         }
     )
 ]
