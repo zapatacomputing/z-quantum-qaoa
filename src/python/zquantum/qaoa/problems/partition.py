ff195315b850757132c853566f2e3335807d3cf3
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-03-23 11:23:18 +0100

    Fix operator term order

--- src/python/zquantum/qaoa/problems/partition.py
+++ src/python/zquantum/qaoa/problems/partition.py
@@ -9,9 +9,16 @@ def _adjacency_matrix(graph: nx.Graph) -> np.ndarray:
     return nx.to_numpy_array(graph)
 
 
+def _qiskit_to_zquantum_matrix(weights_matrix: np.ndarray):
+    """Terms returned by Qiskit have flipped ordering compared to what we'd expect."""
+    return np.flip(weights_matrix)
+
+
 def get_graph_partition_hamiltonian(graph: nx.Graph) -> openfermion.QubitOperator:
     weight_matrix = _adjacency_matrix(graph)
-    qiskit_operator, offset = graph_partition.get_operator(weight_matrix)
+    qiskit_operator, offset = graph_partition.get_operator(
+        _qiskit_to_zquantum_matrix(weight_matrix)
+    )
     openfermion_operator = qiskitpauli_to_qubitop(qiskit_operator)
     # openfermion's QubitOperator doesn't store the offset, we also don't have any
     # other convenient place to keep track of it, so we're adding it as a free term.
