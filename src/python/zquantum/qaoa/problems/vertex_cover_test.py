a7adc4cf4e22560e5dcf471a99e71b2dab391111
simonwa7 <simonwa7@gmail.com>
2021-04-08 16:58:57 -0400

    Adding vertex cover problem with tests

--- src/python/zquantum/qaoa/problems/vertex_cover_test.py
+++ src/python/zquantum/qaoa/problems/vertex_cover_test.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import pytest
 
-from .partition import get_graph_partition_hamiltonian
+from .vertex_cover import get_vertex_cover_hamiltonian
 
 
 def _make_graph(node_ids, edges):
@@ -15,45 +15,48 @@ MONOTONIC_GRAPH_OPERATOR_TERM_PAIRS = [
     (
         _make_graph(node_ids=range(2), edges=[(0, 1)]),
         {
-            (): 2.5 + 0j,
-            ((0, "Z"), (1, "Z")): 1.5 + 0j,
+            (): 2.25 + 0j,
+            ((0, "Z"),): -0.75 + 0j,
+            ((1, "Z"),): -0.75 + 0j,
+            ((0, "Z"), (1, "Z")): 1.25 + 0j,
         },
     ),
     (
         _make_graph(node_ids=range(3), edges=[(0, 1), (0, 2)]),
         {
             (): 4 + 0j,
-            ((0, "Z"), (1, "Z")): 1.5 + 0j,
-            ((0, "Z"), (2, "Z")): 1.5 + 0j,
-            ((1, "Z"), (2, "Z")): 2.0 + 0j,
+            ((0, "Z"),): -2 + 0j,
+            ((1, "Z"),): -0.75 + 0j,
+            ((2, "Z"),): -0.75 + 0j,
+            ((0, "Z"), (1, "Z")): 1.25 + 0j,
+            ((0, "Z"), (2, "Z")): 1.25 + 0j,
         },
     ),
     (
         _make_graph(node_ids=range(4), edges=[(0, 1), (0, 2), (0, 3)]),
         {
-            (): 5.5 + 0j,
-            ((0, "Z"), (1, "Z")): 1.5 + 0j,
-            ((0, "Z"), (2, "Z")): 1.5 + 0j,
-            ((0, "Z"), (3, "Z")): 1.5 + 0j,
-            ((1, "Z"), (2, "Z")): 2 + 0j,
-            ((1, "Z"), (3, "Z")): 2 + 0j,
-            ((2, "Z"), (3, "Z")): 2 + 0j,
+            (): 5.75 + 0j,
+            ((0, "Z"),): -3.25 + 0j,
+            ((1, "Z"),): -0.75 + 0j,
+            ((2, "Z"),): -0.75 + 0j,
+            ((3, "Z"),): -0.75 + 0j,
+            ((0, "Z"), (1, "Z")): 1.25 + 0j,
+            ((0, "Z"), (2, "Z")): 1.25 + 0j,
+            ((0, "Z"), (3, "Z")): 1.25 + 0j,
         },
     ),
     (
         _make_graph(node_ids=range(5), edges=[(0, 1), (1, 2), (3, 4)]),
         {
-            (): 6.5 + 0j,
-            ((0, "Z"), (1, "Z")): 1.5 + 0j,
-            ((0, "Z"), (2, "Z")): 2 + 0j,
-            ((0, "Z"), (3, "Z")): 2 + 0j,
-            ((0, "Z"), (4, "Z")): 2 + 0j,
-            ((1, "Z"), (2, "Z")): 1.5 + 0j,
-            ((1, "Z"), (3, "Z")): 2 + 0j,
-            ((1, "Z"), (4, "Z")): 2 + 0j,
-            ((2, "Z"), (3, "Z")): 2 + 0j,
-            ((2, "Z"), (4, "Z")): 2 + 0j,
-            ((3, "Z"), (4, "Z")): 1.5 + 0j,
+            (): 6.25 + 0j,
+            ((0, "Z"),): -0.75 + 0j,
+            ((1, "Z"),): -2 + 0j,
+            ((2, "Z"),): -0.75 + 0j,
+            ((3, "Z"),): -0.75 + 0j,
+            ((4, "Z"),): -0.75 + 0j,
+            ((0, "Z"), (1, "Z")): 1.25 + 0j,
+            ((1, "Z"), (2, "Z")): 1.25 + 0j,
+            ((3, "Z"), (4, "Z")): 1.25 + 0j,
         },
     ),
 ]
@@ -62,17 +65,21 @@ NONMONOTONIC_GRAPH_OPERATOR_TERM_PAIRS = [
     (
         _make_graph(node_ids=[4, 2], edges=[(2, 4)]),
         {
-            (): 2.5 + 0j,
-            ((0, "Z"), (1, "Z")): 1.5 + 0j,
+            (): 2.25 + 0j,
+            ((0, "Z"),): -0.75 + 0j,
+            ((1, "Z"),): -0.75 + 0j,
+            ((0, "Z"), (1, "Z")): 1.25 + 0j,
         },
     ),
     (
         _make_graph(node_ids="CBA", edges=[("C", "B"), ("C", "A")]),
         {
             (): 4 + 0j,
-            ((0, "Z"), (1, "Z")): 1.5 + 0j,  # the C-B edge
-            ((0, "Z"), (2, "Z")): 1.5 + 0j,  # the C-A edge
-            ((1, "Z"), (2, "Z")): 2.0 + 0j,  # the B-C edge
+            ((0, "Z"),): -2 + 0j,
+            ((1, "Z"),): -0.75 + 0j,
+            ((2, "Z"),): -0.75 + 0j,
+            ((0, "Z"), (1, "Z")): 1.25 + 0j,
+            ((0, "Z"), (2, "Z")): 1.25 + 0j,
         },
     ),
 ]
@@ -104,7 +111,7 @@ def _graph_node_index(graph, node_id):
     return next(node_i for node_i, node in enumerate(graph.nodes) if node == node_id)
 
 
-class TestGetGraphPartitionHamiltonian:
+class TestGetVertexCoverHamiltonian:
     @pytest.mark.parametrize(
         "graph,terms",
         [
@@ -113,18 +120,33 @@ class TestGetGraphPartitionHamiltonian:
         ],
     )
     def test_returns_expected_terms(self, graph, terms):
-        qubit_operator = get_graph_partition_hamiltonian(graph)
+        qubit_operator = get_vertex_cover_hamiltonian(graph)
         assert qubit_operator.terms == terms
 
     @pytest.mark.parametrize("graph", GRAPH_EXAMPLES)
-    def test_has_1_5_weight_on_edge_terms(self, graph: nx.Graph):
-        qubit_operator = get_graph_partition_hamiltonian(graph)
+    def test_has_1_25_weight_on_edge_terms(self, graph: nx.Graph):
+        qubit_operator = get_vertex_cover_hamiltonian(graph)
 
         for vertex_id1, vertex_id2 in graph.edges:
             qubit_index1 = _graph_node_index(graph, vertex_id1)
             qubit_index2 = _graph_node_index(graph, vertex_id2)
             assert (
                 qubit_operator.terms[((qubit_index1, "Z"), (qubit_index2, "Z"))]
-                == 1.5 + 0j
+                == 1.25 + 0j
             )
 
+    @pytest.mark.parametrize("graph", GRAPH_EXAMPLES)
+    def test_has_correct_offset(self, graph: nx.Graph):
+        expected_offset = 0
+
+        qubit_operator = get_vertex_cover_hamiltonian(graph)
+        for vertex_id1, vertex_id2 in graph.edges:
+            expected_offset += 5/4
+        
+        for vertex in graph.nodes:
+            expected_offset += 0.5
+
+        assert (
+                qubit_operator.terms[()]
+                == expected_offset + 0j
+            )
