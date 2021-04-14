4c9c0d9bbf752e997ec3f4255ef1a00696b15e41
mstechly <michal.stechly@gmail.com>
2021-04-13 21:30:35 -0400

    fixup! Add stable set problem.

--- src/python/zquantum/qaoa/problems/__init__.py
+++ src/python/zquantum/qaoa/problems/__init__.py
@@ -16,4 +16,10 @@ from .vertex_cover import (
     solve_vertex_cover_by_exhaustive_search,
 )
 
+from .stable_set import (
+    get_stable_set_hamiltonian,
+    evaluate_stable_set_solution,
+    solve_stable_set_by_exhaustive_search,
+)
+
 from .generators import get_random_hamiltonians_for_problem
