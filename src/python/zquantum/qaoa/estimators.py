58acb9946120ecbe079898018c01eec06cc6d596
mstechly <michal.stechly@gmail.com>
2021-04-21 15:40:48 -0400

    Updated docstrings and comments.

--- src/python/zquantum/qaoa/estimators.py
+++ src/python/zquantum/qaoa/estimators.py
@@ -14,7 +14,7 @@ from zquantum.core.wip.estimators.estimation_interface import (
 class CvarEstimator(EstimateExpectationValues):
     """An estimator for calculating expectation value using CVaR method.
     The main idea is that for diagonal operators the ground state of the Hamiltonian is a base state.
-    In particular for the combinatorial optimization problems, we care only about getting a single bitstring representing the solution.
+    In particular for the combinatorial optimization problems, we often care only about getting a single bitstring representing the solution.
     Therefore, it might be beneficial for the optimization process to discard samples that represent inferior solutions.
     CVaR Estimator takes uses only a top X percentile of the samples for calculating the expectation value, where X = alpha*100.
 
@@ -30,20 +30,12 @@ class CvarEstimator(EstimateExpectationValues):
         self, backend: QuantumBackend, estimation_tasks: List[EstimationTask]
     ) -> List[ExpectationValues]:
         """Given a circuit, backend, and target operators, this method produces expectation values
-        using CVaR algorithm.
-        TODO
+        using CVaR method.
+
         Args:
-            backend (QuantumBackend): the backend that will be used to run the circuit
-            circuit (Circuit): the circuit that prepares the state.
-            target_operator (SymbolicOperator): Operator to be estimated.
-            alpha (float): defines what part of the best measurements should be taken into account in the estimation process.
-            n_samples (int): Number of measurements done on the unknown quantum state.
-
-        Raises:
-            AttributeError: If backend is not a QuantumSimulator.
-
-        Returns:
-            ExpectationValues: expectation values for each term in the target operator.
+            backend: the backend that will be used to run the circuits
+            estimation_tasks: the estimation tasks defining the problem. Each task consist of target operator, circuit and number of shots.
+            alpha: defines what part of the measurements should be taken into account in the estimation process.
         """
         if self.alpha > 1 or self.alpha <= 0:
             raise ValueError("alpha needs to be a value between 0 and 1.")
@@ -71,25 +63,29 @@ class CvarEstimator(EstimateExpectationValues):
 def _calculate_expectation_value_for_distribution(
     distribution: BitstringDistribution, operator: IsingOperator, alpha: float
 ):
-    expected_values_per_bitstring = {}
+    # Calculates expectation value per bitstring
+    expectation_values_per_bitstring = {}
     for bitstring in distribution.distribution_dict:
         expected_value = Measurements(bitstring).get_expectation_values(operator)
-        expected_values_per_bitstring[bitstring] = np.sum(expected_value.values)
+        expectation_values_per_bitstring[bitstring] = np.sum(expected_value.values)
 
-    sorted_expected_values_per_bitstring_list = sorted(
-        expected_values_per_bitstring.items(), key=lambda item: item[1]
+    # Sorts expectation values by values.
+    sorted_expectation_values_per_bitstring_list = sorted(
+        expectation_values_per_bitstring.items(), key=lambda item: item[1]
     )
 
     cumulative_prob = 0.0
     cumulative_value = 0.0
-
-    for bitstring, energy in sorted_expected_values_per_bitstring_list:
+    # Sums expectation values for each bitstring, starting from the one with the smallest one.
+    # When the cumulative probability associated with these bitstrings is higher than alpha,
+    # it stops and effectively discards all the remaining values.
+    for bitstring, expectation_value in sorted_expectation_values_per_bitstring_list:
         prob = distribution.distribution_dict[bitstring]
         if cumulative_prob + prob < alpha:
             cumulative_prob += prob
-            cumulative_value += prob * energy
+            cumulative_value += prob * expectation_value
         else:
-            cumulative_value += (alpha - cumulative_prob) * energy
+            cumulative_value += (alpha - cumulative_prob) * expectation_value
             break
     final_value = cumulative_value / alpha
     return final_value
