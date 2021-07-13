0742b9bc85a6d343b02c587bdb1a4644a8f6b6b0
Laura Gao <lauragao86868@gmail.com>
2021-07-13 17:12:22 -0400

    create gibbs estimator + tests and move estimators to their own folder

---
+++ src/python/zquantum/qaoa/estimators/gibbs_estimator.py
@@ -0,0 +1,86 @@
+import numpy as np
+from openfermion import IsingOperator
+from typing import List
+
+from zquantum.core.interfaces.backend import QuantumBackend
+from zquantum.core.measurement import ExpectationValues, Measurements
+from zquantum.core.bitstring_distribution import BitstringDistribution
+from zquantum.core.interfaces.estimation import (
+    EstimateExpectationValues,
+    EstimationTask,
+)
+
+
+class GibbsEstimator(EstimateExpectationValues):
+    """An estimator for calculating expectation value using the Gibbs objective function method
+    The main idea is that we exponentiate the negative expectation value of each sample, which amplifies bitstrings with
+    low energies while reducing the role that high energy bitstrings play in determining the cost.
+
+    Reference: https://arxiv.org/abs/1909.07621
+    "Quantum Optimization with a Novel Gibbs Objective Function and Ansatz Architecture Search", L. Li, M. Fan, M. Coram, P. Riley, and S. Leichenauer
+    """
+
+    def __init__(self, alpha: float) -> None:
+        super().__init__()
+        self.alpha = alpha
+
+    def __call__(
+        self, backend: QuantumBackend, estimation_tasks: List[EstimationTask]
+    ) -> List[ExpectationValues]:
+        """Given a circuit, backend, and target operators, this method produces expectation values
+        using Gibbs method.
+
+        Args:
+            backend: the backend that will be used to run the circuits
+            estimation_tasks: the estimation tasks defining the problem. Each task consist of target operator, circuit and number of shots.
+            alpha: defines to what degree will expectation values be exponentiated to. exp(-alpha * expectation_value)
+        """
+        if self.alpha <= 0:
+            raise ValueError("alpha needs to be a value greater than 0.")
+
+        circuits, operators, shots_per_circuit = zip(
+            *[(e.circuit, e.operator, e.number_of_shots) for e in estimation_tasks]
+        )
+        distributions_list = [
+            backend.get_bitstring_distribution(circuit, n_samples=n_shots)
+            for circuit, n_shots in zip(circuits, shots_per_circuit)
+        ]
+
+        return [
+            ExpectationValues(
+                np.array(
+                    [
+                        _calculate_expectation_value_for_distribution(
+                            distribution, operator, self.alpha
+                        )
+                    ]
+                )
+            )
+            for distribution, operator in zip(distributions_list, operators)
+        ]
+
+
+def _calculate_expectation_value_for_distribution(
+    distribution: BitstringDistribution, operator: IsingOperator, alpha: float
+) -> float:
+
+    # Calculates expectation value per bitstring
+    expectation_values_per_bitstring = {}
+    for bitstring in distribution.distribution_dict:
+        expected_value = Measurements([bitstring]).get_expectation_values(
+            operator, use_bessel_correction=False
+        )
+        expectation_values_per_bitstring[bitstring] = np.sum(expected_value.values)
+
+    cumulative_value = 0.0
+    # Get total expectation value (mean of expectation values of all bitstrings weighted by distribution)
+    for bitstring in expectation_values_per_bitstring:
+        prob = distribution.distribution_dict[bitstring]
+
+        # for the i-th sampled bitstring, compute exp(-alpha E_i)
+        expectation_value = np.exp(-alpha * expectation_values_per_bitstring[bitstring])
+        cumulative_value += prob * expectation_value
+
+    final_value = -np.log(cumulative_value)
+
+    return final_value
--- tests/zquantum/qaoa/estimators/gibbs_estimator_test.py
+++ tests/zquantum/qaoa/estimators/gibbs_estimator_test.py
@@ -6,15 +6,16 @@ from zquantum.core.interfaces.mock_objects import (
 )
 from zquantum.core.circuits import Circuit, X, H
 from zquantum.core.measurement import Measurements
-from zquantum.qaoa.estimators import CvarEstimator
+from zquantum.qaoa.estimators.gibbs_estimator import GibbsEstimator
 
 from zquantum.core.interfaces.estimation import EstimationTask
+import numpy as np
 
 
-class TestCvarEstimator:
+class TestGibbsEstimator:
     @pytest.fixture(params=[1.0, 0.8, 0.5, 0.2])
     def estimator(self, request):
-        return CvarEstimator(alpha=request.param)
+        return GibbsEstimator(alpha=request.param)
 
     @pytest.fixture()
     def circuit(self):
@@ -50,7 +51,7 @@ class TestCvarEstimator:
                 estimation_tasks=estimation_tasks,
             )
 
-    def test_cvar_estimator_raises_exception_if_alpha_less_than_0(
+    def test_gibbs_estimator_raises_exception_if_alpha_less_than_0(
         self, estimator, backend, estimation_tasks
     ):
         estimator.alpha = -1
@@ -60,23 +61,15 @@ class TestCvarEstimator:
                 estimation_tasks=estimation_tasks,
             )
 
-    def test_cvar_estimator_raises_exception_if_alpha_greater_than_1(
-        self, estimator, backend, estimation_tasks
-    ):
-        estimator.alpha = 2
-        with pytest.raises(ValueError):
-            estimator(
-                backend=backend,
-                estimation_tasks=estimation_tasks,
-            )
-
-    def test_cvar_estimator_returns_correct_values(self, estimator, backend, operator):
+    def test_gibbs_estimator_returns_correct_values(self, estimator, backend, operator):
         # Given
         estimation_tasks = [EstimationTask(operator, Circuit([H(0)]), 10)]
-        if estimator.alpha <= 0.5:
-            target_value = -1
-        else:
-            target_value = (-1 * 0.5 + 1 * (estimator.alpha - 0.5)) / estimator.alpha
+
+        expval_0 = np.exp(1 * -estimator.alpha)  # Expectation value of bitstring 0
+        expval_1 = np.exp(-1 * -estimator.alpha)  # Expectation value of bitstring 0
+
+        # Target value is the -log of the mean of the expectation values of the 2 bitstrings
+        target_value = -np.log((expval_1 + expval_0) / 2)
 
         # When
         expectation_values = estimator(
