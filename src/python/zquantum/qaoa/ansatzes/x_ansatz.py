3f63697bfee1d7e9df569c1352d60dc9c5563d66
Laura Gao <lauragao86868@gmail.com>
2021-07-14 16:27:53 -0400

    put X and XZ in the same file, make test unitaries more concise

--- src/python/zquantum/qaoa/ansatzes/x_ansatz.py
+++ src/python/zquantum/qaoa/ansatzes/x_ansatz.py
@@ -2,54 +2,49 @@ from zquantum.core.interfaces.ansatz import Ansatz, ansatz_property
 
 from zquantum.core.circuits import Circuit
 from zquantum.core.evolution import time_evolution
-from zquantum.core.openfermion import change_operator_type
-
 from openfermion import QubitOperator, IsingOperator
-from openfermion.utils import count_qubits
-from typing import Tuple, Union, Optional, List
+from typing import Union, Optional
 import numpy as np
 import sympy
 from overrides import overrides
 from itertools import combinations
 
+import operator as op
+from functools import reduce
 
-class QAOAXAnsatz(Ansatz):
+
+class XAnsatz(Ansatz):
 
     supports_parametrized_circuits = True
-    cost_hamiltonian = ansatz_property("cost_hamiltonian")
+    number_of_qubits = ansatz_property("number_of_qubits")
 
     def __init__(
         self,
         number_of_layers: int,
-        cost_hamiltonian: Union[QubitOperator, IsingOperator],
+        number_of_qubits: Union[QubitOperator, IsingOperator],
     ):
         """This is implementation of the X Ansatz from https://arxiv.org/abs/2105.01114
 
         Args:
-            number_of_layers: k-body depth (the maximum number of qubits entangled at one time) as described in https://arxiv.org/abs/2105.01114. Cannot be greater than the number of qubits.
-            cost_hamiltonian: Hamiltonian representing the cost function
+            number_of_layers: k-body depth (the maximum number of qubits entangled at one time) as described in the original paper. Cannot be greater than the number of qubits.
+            number_of_qubits: number of qubits required for the ansatz circuit.
 
         Attributes:
-            number_of_qubits: number of qubits required for the ansatz circuit.
             number_of_params: number of the parameters that need to be set for the ansatz circuit.
         """
 
         super().__init__(number_of_layers)
-        self._cost_hamiltonian = cost_hamiltonian
+        self.number_of_qubits = number_of_qubits
 
-        assert number_of_layers <= self.number_of_qubits
-
-    @property
-    def number_of_qubits(self):
-        """Returns number of qubits used for the ansatz circuit."""
-        return count_qubits(change_operator_type(self._cost_hamiltonian, QubitOperator))
+        assert number_of_layers <= number_of_qubits
 
     @property
     def number_of_params(self) -> int:
         """Returns number of parameters in the ansatz."""
+        # See section 4.1 of the original paper
         sum = 0
         for i in range(1, self.number_of_layers + 1):
-            sum += ncr(self.number_of_qubits, i)
+            sum += n_choose_r(self.number_of_qubits, i)
         return sum
 
     @overrides
@@ -62,55 +57,101 @@ class QAOAXAnsatz(Ansatz):
             Warning(
                 "This method retuns a parametrizable circuit, params will be ignored."
             )
-        circuit = Circuit()
 
-        # Add time evolution layers
-        j = 0
-        for k in range(self.number_of_layers):
+        return _create_circuit(self.number_of_layers, self.number_of_qubits, "X")
 
-            A = combinations(range(0, self.number_of_qubits), k + 1)
 
-            for S in list(A):
-                H_j = QubitOperator(" ".join([f"X{i}" for i in S]))
+class XZAnsatz(Ansatz):
 
-                circuit += time_evolution(H_j, sympy.Symbol(f"theta_{j}"))
+    supports_parametrized_circuits = True
+    number_of_qubits = ansatz_property("number_of_qubits")
+    use_k_body_z_operators = ansatz_property("use_k_body_z_operators")
+
+    def __init__(
+        self,
+        number_of_layers: int,
+        number_of_qubits: Union[QubitOperator, IsingOperator],
+        use_k_body_z_operators: bool = True,
+    ):
+        """This is implementation of the XZ Ansatzes from https://arxiv.org/abs/2105.01114 section 4.2
+
+        Args:
+            number_of_layers: k-body depth (the maximum number of qubits entangled at one time) as described in https://arxiv.org/abs/2105.01114.  Cannot be greater than the number of qubits.
+            number_of_qubits: number of qubits required for the ansatz circuit.
+            use_k_body_z_operators: from the two types of XZ ansatzes in the original paper
+
+        Attributes:
+            number_of_params: number of the parameters that need to be set for the ansatz circuit.
+        """
+
+        super().__init__(number_of_layers)
+        self.number_of_qubits = number_of_qubits
+        self._use_k_body_z_operators = use_k_body_z_operators
+
+        assert number_of_layers <= number_of_qubits
+
+    @property
+    def number_of_params(self) -> int:
+        """Returns number of parameters in the ansatz."""
+        sum = 0
+        for i in range(1, self.number_of_layers + 1):
+            sum += n_choose_r(self.number_of_qubits, i)
+        return sum * 2
+
+    @overrides
+    def _generate_circuit(self, params: Optional[np.ndarray] = None) -> Circuit:
+        """Returns a parametrizable circuit represention of the ansatz.
+        Args:
+            params: parameters of the circuit.
+        """
+        if params is not None:
+            Warning(
+                "This method retuns a parametrizable circuit, params will be ignored."
+            )
+
+        if self._use_k_body_z_operators:
+            type = "XZ1"
+        else:
+            type = "XZ2"
+        return _create_circuit(self.number_of_layers, self.number_of_qubits, type)
+
+
+def _create_circuit(number_of_layers: int, number_of_qubits: int, type) -> Circuit:
+    """Args:
+    type: X, XZ1, or XZ2
+    """
+    circuit = Circuit()
+    j = 0
+
+    # See equation 12 and figure 3(a) in the original paper.
+    # S is a set of vertices that represents a cut of the graph while
+    # A is the set of all possible cuts.
+    # The maximum number of vertices in each set is the k-body depth.
+    for k in range(number_of_layers):
+        A = combinations(range(0, number_of_qubits), k + 1)
+        for S in list(A):
+            H_j = QubitOperator(" ".join([f"X{i}" for i in S]))
+            circuit += time_evolution(H_j, sympy.Symbol(f"theta_{j}"))
+
+            j += 1
+
+            if type != "X":
+                if type == "XZ1":
+                    # See figure 4(a) in the original paper
+                    H_k = QubitOperator(" ".join([f"Z{i}" for i in S]))
+                elif type == "XZ2":
+                    # See figure 4(b) in the original paper
+                    H_k = QubitOperator()
+                    for i in range(number_of_qubits):
+                        H_k += QubitOperator((i, "Z"))
+
+                circuit += time_evolution(H_k, sympy.Symbol(f"theta_{j}"))
                 j += 1
 
-        return circuit
+    return circuit
 
 
-def create_x_qaoa_circuits(
-    hamiltonians: List[QubitOperator], number_of_layers: Union[int, List[int]]
-):
-    """Creates parameterizable quantum circuits based on the X qaoa ansatz for each
-    hamiltonian in the input list using the set number of layers.
-    Args:
-        hamiltonians (List[QubitOperator]): List of hamiltonians for constructing the
-            circuits
-        number_of_layers (Union[int, List[int]]): The k-body depth of the ansatz in the circuit.
-            If an int is passed in, the same k-body depth is used for every ansatz circuit, however,
-            if a list of ints is passed in, the k-body depth used for the hamiltonian at index i of the hamiltonians
-            list is the integer at index i of the number_of_layers list.
-    Returns:
-        List of zquantum.core.circuit.Circuit
-    """
-    if isinstance(number_of_layers, int):
-        number_of_layers = [number_of_layers for _ in range(len(hamiltonians))]
-    number_of_layers_list = number_of_layers
-    assert len(number_of_layers_list) == len(hamiltonians)
-
-    circuitset = []
-    for number_of_layers, hamiltonian in zip(number_of_layers_list, hamiltonians):
-        ansatz = QAOAXAnsatz(number_of_layers, hamiltonian)
-        circuitset.append(ansatz.parametrized_circuit)
-    return circuitset
-
-
-import operator as op
-from functools import reduce
-
-
-def ncr(n, r):
+def n_choose_r(n: int, r: int) -> int:
     r = min(r, n - r)
     numer = reduce(op.mul, range(n, n - r, -1), 1)
     denom = reduce(op.mul, range(1, r + 1), 1)
--- tests/zquantum/qaoa/ansatzes/x_ansatz_test.py
+++ tests/zquantum/qaoa/ansatzes/x_ansatz_test.py
@@ -1,11 +1,9 @@
 import pytest
 import sympy
-from openfermion import IsingOperator, QubitOperator
 from zquantum.core.circuits import CNOT, RZ, Circuit, H
 from zquantum.core.interfaces.ansatz_test import AnsatzTests
-from zquantum.core.openfermion import change_operator_type
 from zquantum.core.utils import compare_unitary
-from zquantum.qaoa.ansatzes.x_ansatz import QAOAXAnsatz, create_x_qaoa_circuits
+from zquantum.qaoa.ansatzes.x_ansatz import XAnsatz, XZAnsatz
 
 
 def create_thetas(number_of_params):
@@ -20,123 +18,111 @@ def create_symbols_map(number_of_params):
     return symbols_map
 
 
-def create_target_unitary(number_of_params, k_body_depth=1):
+def create_x_operator(qubit: int, param):
+    return Circuit([H(qubit), RZ(2 * param)(qubit), H(qubit)])
+
+
+def create_2_qubit_x_operator(qubit1: int, qubit2: int, param):
+    return Circuit(
+        [
+            H(qubit1),
+            H(qubit2),
+            CNOT(qubit1, qubit2),
+            RZ(2 * param)(qubit2),
+            CNOT(qubit1, qubit2),
+            H(qubit1),
+            H(qubit2),
+        ]
+    )
+
+
+def create_X_target_unitary(number_of_params, k_body_depth=1):
     thetas = create_thetas(number_of_params)
     symbols_map = create_symbols_map(number_of_params)
 
     target_circuit = Circuit()
-    target_circuit += H(0)
-    target_circuit += RZ(2 * thetas[0])(0)
-    target_circuit += H(0)
-    target_circuit += H(1)
-    target_circuit += RZ(2 * thetas[1])(1)
-    target_circuit += H(1)
-    target_circuit += H(2)
-    target_circuit += RZ(2 * thetas[2])(2)
-    target_circuit += H(2)
+    # Add an x operator to every qubit
+    for i in range(3):
+        target_circuit += create_x_operator(i, thetas[i])
 
     if k_body_depth == 2:
-        target_circuit += H(0)
-        target_circuit += H(1)
-        target_circuit += CNOT(0, 1)
-        target_circuit += RZ(2 * thetas[3])(1)
-        target_circuit += CNOT(0, 1)
-        target_circuit += H(1)
-        target_circuit += H(0)
-
-        target_circuit += H(0)
-        target_circuit += H(2)
-        target_circuit += CNOT(0, 2)
-        target_circuit += RZ(2 * thetas[4])(2)
-        target_circuit += CNOT(0, 2)
-        target_circuit += H(0)
-        target_circuit += H(2)
-
-        target_circuit += H(1)
-        target_circuit += H(2)
-        target_circuit += CNOT(1, 2)
-        target_circuit += RZ(2 * thetas[5])(2)
-        target_circuit += CNOT(1, 2)
-        target_circuit += H(2)
-        target_circuit += H(1)
+        target_circuit += create_2_qubit_x_operator(0, 1, thetas[3])
+        target_circuit += create_2_qubit_x_operator(0, 2, thetas[4])
+        target_circuit += create_2_qubit_x_operator(1, 2, thetas[5])
 
     return target_circuit.bind(symbols_map).to_unitary()
 
 
-class TestQAOAXAnsatz(AnsatzTests):
+def create_XZ1_target_unitary(number_of_params, k_body_depth=1):
+    thetas = create_thetas(number_of_params)
+    symbols_map = create_symbols_map(number_of_params)
+
+    target_circuit = Circuit()
+    target_circuit += create_x_operator(0, thetas[0])
+    target_circuit += RZ(2 * thetas[1])(0)
+
+    target_circuit += create_x_operator(1, thetas[2])
+    target_circuit += RZ(2 * thetas[3])(1)
+
+    if k_body_depth == 2:
+        target_circuit += create_2_qubit_x_operator(0, 1, thetas[4])
+        target_circuit += CNOT(0, 1)
+        target_circuit += RZ(2 * thetas[5])(1)
+        target_circuit += CNOT(0, 1)
+
+    return target_circuit.bind(symbols_map).to_unitary()
+
+
+def create_XZ2_target_unitary(number_of_params, k_body_depth=1):
+    thetas = create_thetas(number_of_params)
+    symbols_map = create_symbols_map(number_of_params)
+
+    target_circuit = Circuit()
+    target_circuit += create_x_operator(0, thetas[0])
+    target_circuit += RZ(2 * thetas[1])(0)
+    target_circuit += RZ(2 * thetas[1])(1)
+
+    target_circuit += create_x_operator(1, thetas[2])
+    target_circuit += RZ(2 * thetas[3])(0)
+    target_circuit += RZ(2 * thetas[3])(1)
+
+    if k_body_depth == 2:
+        target_circuit += create_2_qubit_x_operator(0, 1, thetas[4])
+
+        target_circuit += RZ(2 * thetas[5])(0)
+        target_circuit += RZ(2 * thetas[5])(1)
+
+    return target_circuit.bind(symbols_map).to_unitary()
+
+
+class TestXAnsatz(AnsatzTests):
     @pytest.fixture()
     def number_of_params(self):
         return 3
 
     @pytest.fixture
     def ansatz(self):
-        cost_hamiltonian = QubitOperator(("Z0 Z1")) + QubitOperator(("Z1 Z2"))
-        return QAOAXAnsatz(
+        return XAnsatz(
             number_of_layers=1,
-            cost_hamiltonian=cost_hamiltonian,
+            number_of_qubits=3,
         )
 
     @pytest.fixture
     def target_unitary(self, number_of_params):
-        return create_target_unitary(number_of_params)
-
-    def test_get_number_of_qubits(self, ansatz):
-        # Given
-        cost_hamiltonian = QubitOperator(("Z0 Z1")) + QubitOperator(("Z1 Z2"))
-        target_number_of_qubits = 3
-
-        # When
-        ansatz.cost_hamiltonian = cost_hamiltonian
-
-        # Then
-        assert ansatz.number_of_qubits == target_number_of_qubits
-
-    def test_get_number_of_qubits_with_ising_hamiltonian(self, ansatz):
-        # Given
-        new_cost_hamiltonian = (
-            QubitOperator((0, "Z")) + QubitOperator((1, "Z")) + QubitOperator((2, "Z"))
-        )
-        new_cost_hamiltonian = change_operator_type(new_cost_hamiltonian, IsingOperator)
-        target_number_of_qubits = 3
-
-        # When
-        ansatz.cost_hamiltonian = new_cost_hamiltonian
-
-        # Then
-        assert ansatz.number_of_qubits == target_number_of_qubits
+        return create_X_target_unitary(number_of_params)
 
     def test_get_number_of_params(self, ansatz):
         # Given
-        cost_hamiltonian = QubitOperator(("Z0 Z1")) + QubitOperator(("Z1 Z2"))
         target_number_of_params = 3
-
-        # When
-        ansatz.cost_hamiltonian = cost_hamiltonian
-
-        # Then
-        assert ansatz.number_of_params == target_number_of_params
-
-    def test_get_number_of_params_with_ising_hamiltonian(self, ansatz):
-        # Given
-        new_cost_hamiltonian = (
-            QubitOperator((0, "Z")) + QubitOperator((1, "Z")) + QubitOperator((2, "Z"))
-        )
-        new_cost_hamiltonian = change_operator_type(new_cost_hamiltonian, IsingOperator)
-        target_number_of_params = 3
-
-        # When
-        ansatz.cost_hamiltonian = new_cost_hamiltonian
 
         # Then
         assert ansatz.number_of_params == target_number_of_params
 
     def test_get_number_of_params_with_k_body_depth_greater_than_1(self, ansatz):
         # Given
-        cost_hamiltonian = QubitOperator(("Z0 Z1")) + QubitOperator(("Z1 Z2"))
         target_number_of_params = 6
 
         # When
-        ansatz.cost_hamiltonian = cost_hamiltonian
         ansatz.number_of_layers = 2
 
         # Then
@@ -155,7 +141,7 @@ class TestQAOAXAnsatz(AnsatzTests):
     def test_generate_circuit_with_k_body_depth_greater_than_1(self, ansatz):
         # When
         symbols_map = create_symbols_map(number_of_params=6)
-        target_unitary = create_target_unitary(number_of_params=6, k_body_depth=2)
+        target_unitary = create_X_target_unitary(number_of_params=6, k_body_depth=2)
         ansatz.number_of_layers = 2
         parametrized_circuit = ansatz._generate_circuit()
         evaluated_circuit = parametrized_circuit.bind(symbols_map)
@@ -165,32 +151,88 @@ class TestQAOAXAnsatz(AnsatzTests):
         assert compare_unitary(final_unitary, target_unitary, tol=1e-10)
 
 
-def test_create_x_qaoa_circuits():
-    # Given
-    hamiltonians = [
-        QubitOperator("Z0 Z1"),
-        QubitOperator("Z0") + QubitOperator("Z1"),
-    ]
-    number_of_layers = 2
+class TestXZAnsatz(AnsatzTests):
+    @pytest.fixture()
+    def number_of_params(self):
+        return 4
 
-    # When
-    circuits = create_x_qaoa_circuits(hamiltonians, number_of_layers)
+    @pytest.fixture
+    def ansatz(self):
+        return XZAnsatz(
+            number_of_layers=1,
+            number_of_qubits=2,
+        )
 
-    # Then
-    assert len(circuits) == len(hamiltonians)
+    def test_get_number_of_params(self, ansatz):
+        # Given
+        target_number_of_params = 4
 
-    for circuit in circuits:
-        assert isinstance(circuit, Circuit)
+        # Then
+        assert ansatz.number_of_params == target_number_of_params
 
+    def test_get_number_of_params_with_k_body_depth_greater_than_1(self, ansatz):
+        # Given
+        target_number_of_params = 6
 
-def test_create_x_qaoa_circuits_fails_when_length_of_inputs_is_not_equal():
-    # Given
-    hamiltonians = [
-        QubitOperator("Z0 Z1"),
-        QubitOperator("Z0") + QubitOperator("Z1"),
-    ]
-    number_of_layers = [2]
+        # When
+        ansatz.number_of_layers = 2
 
-    # When
-    with pytest.raises(AssertionError):
-        create_x_qaoa_circuits(hamiltonians, number_of_layers)
+        # Then
+        assert ansatz.number_of_params == target_number_of_params
+
+    def test_generate_circuit(self, ansatz):
+        # Given
+        symbols_map = create_symbols_map(number_of_params=4)
+        target_unitary = create_XZ1_target_unitary(number_of_params=4)
+
+        # When
+        parametrized_circuit = ansatz._generate_circuit()
+        evaluated_circuit = parametrized_circuit.bind(symbols_map)
+        final_unitary = evaluated_circuit.to_unitary()
+
+        # Then
+        assert compare_unitary(final_unitary, target_unitary, tol=1e-10)
+
+    def test_generate_circuit_with_k_body_depth_greater_than_1(self, ansatz):
+        # Given
+        symbols_map = create_symbols_map(number_of_params=6)
+        target_unitary = create_XZ1_target_unitary(number_of_params=6, k_body_depth=2)
+
+        ansatz.number_of_layers = 2
+
+        # When
+        parametrized_circuit = ansatz._generate_circuit()
+        evaluated_circuit = parametrized_circuit.bind(symbols_map)
+        final_unitary = evaluated_circuit.to_unitary()
+
+        # Then
+        assert compare_unitary(final_unitary, target_unitary, tol=1e-10)
+
+    def test_generate_circuit_type_2(self, ansatz):
+        # Given
+        symbols_map = create_symbols_map(number_of_params=4)
+        target_unitary = create_XZ2_target_unitary(number_of_params=4)
+
+        ansatz.use_k_body_z_operators = False
+
+        # When
+        parametrized_circuit = ansatz._generate_circuit()
+        evaluated_circuit = parametrized_circuit.bind(symbols_map)
+        final_unitary = evaluated_circuit.to_unitary()
+        # Then
+        assert compare_unitary(final_unitary, target_unitary, tol=1e-10)
+
+    def test_generate_circuit_type_2_with_k_body_depth_greater_than_1(self, ansatz):
+        # Given
+        symbols_map = create_symbols_map(number_of_params=6)
+        target_unitary = create_XZ2_target_unitary(number_of_params=6, k_body_depth=2)
+        ansatz.number_of_layers = 2
+        ansatz.use_k_body_z_operators = False
+
+        # When
+        parametrized_circuit = ansatz._generate_circuit()
+        evaluated_circuit = parametrized_circuit.bind(symbols_map)
+        final_unitary = evaluated_circuit.to_unitary()
+
+        # Then
+        assert compare_unitary(final_unitary, target_unitary, tol=1e-10)
