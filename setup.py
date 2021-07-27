65ace35b602d0847f77ca687b40c5a73096d735b
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-07-27 15:19:52 +0200

    ZQS-512 use git to determine project version

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
-import os
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="z-quantum-qaoa",
-    version="0.2.0",
+    use_scm_version=True,
     author="Zapata Computing, Inc.",
     author_email="info@zapatacomputing.com",
     description="QAOA implementation for Orquestra.",
@@ -21,5 +20,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
+    setup_requires=["setuptools_scm~=6.0"],
     install_requires=["z-quantum-core"],
 )
