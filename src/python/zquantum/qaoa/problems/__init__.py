e1da1fab514ecf4d2fd1667466d31c982f46248d
mathmeetsmusic <mathmeetsmusic@gmail.com>
2021-11-04 17:32:27 -0400

    StableSet -> MaxIndependentSet in Problems/__init__

--- src/python/zquantum/qaoa/problems/__init__.py
+++ src/python/zquantum/qaoa/problems/__init__.py
@@ -2,5 +2,5 @@ from ._problem_evaluation import solve_problem_by_exhaustive_search
 from .generators import get_random_hamiltonians_for_problem
 from .graph_partition import GraphPartitioning
 from .maxcut import MaxCut
-from .stable_set import StableSet
+from .max_indepenent_set import MaxIndependentSet
 from .vertex_cover import VertexCover
