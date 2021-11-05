bcacd35f90e64fe90678034a6edef1b52f50b2c5
mathmeetsmusic <mathmeetsmusic@gmail.com>
2021-11-05 12:51:53 -0400

    Added B variable & modified class docstring

--- src/python/zquantum/qaoa/problems/vertex_cover.py
+++ src/python/zquantum/qaoa/problems/vertex_cover.py
@@ -5,14 +5,24 @@ from .problem import Problem
 
 
 class VertexCover(Problem):
-    def __init__(self, hamiltonian_factor: int = 5):
-        self._hamiltonian_factor = hamiltonian_factor
+    """Solves vertex cover problem. 
+    
+    The solution of a vertex cover problem is the minimal number of colored
+    verticies such that all edges connect to a colored vertex.
+    
+    Attributes:
+        _A: Hard constraint with huge penalty ensuring output is a solution.
+        _B: Cost of coloring a particular vertex.
+    """
+    def __init__(self, A: int = 5, B: int = 1):
+        self._A = A
+        self._B = B
 
     def _build_hamiltonian(
         self,
         graph: nx.Graph,
     ) -> QubitOperator:
-        """Construct a qubit operator with Hamiltonian for the vertex cover problem.
+        """Construct a Hamiltonian for the vertex cover problem.
 
         From "Ising formulations of many NP Problems" by A. Lucas, eq. 33 and 34
 		(https://arxiv.org/pdf/1302.5843.pdf)
@@ -27,17 +37,15 @@ class VertexCover(Problem):
         Args:
             graph: undirected weighted graph defining the problem
 
-        Returns:
-            operator describing the Hamiltonian
         """  # noqa: E501
         ham_a = QubitOperator()
         for i, j in graph.edges:
             ham_a += (1 - QubitOperator(f"Z{i}")) * (1 - QubitOperator(f"Z{j}"))
-        ham_a *= self._hamiltonian_factor / 4
+        ham_a *= self._A / 4
 
         ham_b = QubitOperator()
         for i in graph.nodes:
             ham_b += QubitOperator(f"Z{i}")
-        ham_b /= 2
+        ham_b *= self._B / 2
 
-        return ham_a + ham_b + len(graph.nodes) / 2
+        return ham_a + ham_b + len(graph.nodes) * self._B / 2
