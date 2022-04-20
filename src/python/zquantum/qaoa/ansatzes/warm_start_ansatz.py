df17051ffde822259b8d1266b6f2462f79fb9086
Shiro-Raven <exclass9.24@gmail.com>
2022-04-20 16:26:01 +0200

    fix issue regarding circuit generators

--- src/python/zquantum/qaoa/ansatzes/warm_start_ansatz.py
+++ src/python/zquantum/qaoa/ansatzes/warm_start_ansatz.py
@@ -72,7 +72,9 @@ class WarmStartQAOAAnsatz(Ansatz):
         circuit = Circuit()
 
         # Prepare initial state
-        circuit += create_layer_of_gates(self.number_of_qubits, RY, self._thetas)
+        circuit += create_layer_of_gates(
+            self.number_of_qubits, RY, self._thetas.reshape(-1, 1)
+        )
 
         # Add time evolution layers
         cost_circuit = time_evolution(
@@ -83,13 +85,19 @@ class WarmStartQAOAAnsatz(Ansatz):
             circuit += cost_circuit.bind(
                 {sympy.Symbol("gamma"): sympy.Symbol(f"gamma_{i}")}
             )
-            circuit += create_layer_of_gates(self.number_of_qubits, RY, -self._thetas)
+            circuit += create_layer_of_gates(
+                self.number_of_qubits, RY, -self._thetas.reshape(-1, 1)
+            )
             circuit += create_layer_of_gates(
                 self.number_of_qubits,
                 RZ,
-                np.array([-2 * sympy.Symbol(f"beta_{i}")] * self.number_of_qubits),
+                np.array(
+                    [-2 * sympy.Symbol(f"beta_{i}")] * self.number_of_qubits
+                ).reshape(-1, 1),
             )
-            circuit += create_layer_of_gates(self.number_of_qubits, RY, self._thetas)
+            circuit += create_layer_of_gates(
+                self.number_of_qubits, RY, self._thetas.reshape(-1, 1)
+            )
 
         return circuit
 
